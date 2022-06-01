7881d6828cc46122482cde1e3d89e1785c280d1d
Leonardo Amorim Ramos <leonardo.ramos-ext@ab-inbev.com>
2022-05-31 22:02:36 -0300

    scripts

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<title>DESCUBRA O CARTÃO DE CRÉDITO IDEAL PARA VOCÊ!</title>
+</head>
+<body>
+
+	<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>

0a2a994359efcf539ff51d0cf2e49c928d8faf0f
Leonardo Amorim Ramos <leonardo.ramos-ext@ab-inbev.com>
2022-06-23 20:50:34 -0300

    ajustes no layout responsivo

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <img class="logo" 
     	src="images/logo2.jpeg" alt="">
     </div>
-	<div class="container col-md-4">       
+	<div class="container col-md-6 col-xl-4 col-lg-8">       
         <?php include 'sections/first_section.php'; ?>
 		<?php include 'sections/second_section.php'; ?>
 		<?php include 'sections/third_section.php'; ?>
